700ef264a5c9ba73d2969e24516595cfa0a3bf40
Angela <antunezangela89@gmail.com>
2023-12-15 12:59:21 +0100

    Get all the characters (20)

--- axios.js
+++ axios.js
@@ -18,20 +18,53 @@ const charactersContainer = document.getElementById('charactersContainer');
 
 //extraer personaje
 
+// function getCharacters() {
+//     //insertar endpoint
+//     axios.get("https://rickandmortyapi.com/api/character")
+//         .then((response) => {
+//             //vamos a extraer el personaje e insertarlo
+//             const character = response.data.results[9]
+//             console.log(character);
+
+//             //vamos a insertar el personaje
+//             charactersContainer.innerHTML = `<div>
+//             <img src="${character.image}">
+//             <h2>${character.name}</h2>
+//             </div>
+//             `
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         })
+// }
+// getCharacters();
+
+//necesitamos recorrer el array para ir sacando la info de los personajes
+
 function getCharacters() {
     //insertar endpoint
     axios.get("https://rickandmortyapi.com/api/character")
         .then((response) => {
-            //vamos a extraer el personaje e insertarlo
-            const character = response.data.results[9]
-            console.log(character);
+            //vamos a extraer un personaje e insertarlo
+            const characters = response.data.results
+            console.log(characters);
 
-            //vamos a insertar el personaje
-            charactersContainer.innerHTML = `<div>
-            <img src="${character.image}">
-            <h2>${character.name}</h2>
-            </div>
-            `
+            //recorrer el array
+            characters.forEach(character => {
+                //por cada elemento del array, vas a ejecutar una serie de instrucciones
+
+                //crear la estructura de los datos => recogerlo en una const
+                const card = `<div>
+                <img src="${character.image}">
+                <h2>${character.name}</h2>
+                </div>`
+
+                //vamos a insertar cada personaje
+                charactersContainer.innerHTML += card;
+
+            });
+
+
         })
         .catch((err) => {
             console.log(err);
