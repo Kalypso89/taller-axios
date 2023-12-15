c92b7c8039b7e79db62aac9fe6158a1bee8419a2
Angela <antunezangela89@gmail.com>
2023-12-15 12:56:56 +0100

    Get only one character

--- axios.js
+++ axios.js
@@ -3,15 +3,39 @@ console.log("Está funcionando");
 //llamar al contenedor que hemos creado en HTML para poder manejarlo
 const charactersContainer = document.getElementById('charactersContainer');
 
-//crear la función para hacer la llamada a la API con axios
+// //crear la función para hacer la llamada a la API con axios
+// function getCharacters() {
+//     //insertar endpoint
+//     axios.get("https://rickandmortyapi.com/api/character")
+//         .then((response) => {
+//             console.log(response.data.results);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         })
+// }
+// getCharacters();
+
+//extraer personaje
+
 function getCharacters() {
     //insertar endpoint
-    axios.get("https://rickandmortyapi.com/api/charact")
+    axios.get("https://rickandmortyapi.com/api/character")
         .then((response) => {
-            console.log(response.data.results);
+            //vamos a extraer el personaje e insertarlo
+            const character = response.data.results[9]
+            console.log(character);
+
+            //vamos a insertar el personaje
+            charactersContainer.innerHTML = `<div>
+            <img src="${character.image}">
+            <h2>${character.name}</h2>
+            </div>
+            `
         })
         .catch((err) => {
             console.log(err);
         })
 }
 getCharacters();
+
